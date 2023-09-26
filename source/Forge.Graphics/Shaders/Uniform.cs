b8f463d6b7e0da95994b627448301815c7b147ef
JillyMan <artyom543211@gmail.com>
2023-09-26 21:24:39 +0200

    Fix vec types accorrding to silk.net and introduce new method.

--- source/Forge.Graphics/Shaders/Uniform.cs
+++ source/Forge.Graphics/Shaders/Uniform.cs
@@ -1,11 +1,12 @@
-﻿using Silk.NET.OpenGL;
-using System.Numerics;
+﻿using Silk.NET.Maths;
+using Silk.NET.OpenGL;
 
 namespace Forge.Graphics.Shaders;
 
 public class Uniform
 {
 	private readonly CompiledShader cs;
+	private readonly GL gl;
 
 	public int Location { get; init; }
 
@@ -18,16 +19,24 @@ public class Uniform
 	public Uniform(CompiledShader cs, int location, string name, UniformType type, int size)
     {
 		this.cs = cs;
+
+		gl = cs.GL;
 		Location = location;
 		Name = name;
 		Type = type;
 		Size = size;
 	}
 
-	public void SetValue(Vector4 value)
+	public void SetValue(Vector3D<float> value)
+	{
+		ValidateType(UniformType.FloatVec3);
+		gl.ProgramUniform3(cs.ProgramId, this.Location, value.X, value.Y, value.Z);
+	}
+
+	public void SetValue(Vector4D<float> value)
 	{
 		ValidateType(UniformType.FloatVec4);
-		cs.GL.ProgramUniform4(cs.ProgramId, this.Location, value.X, value.Y, value.Z, value.W);
+		gl.ProgramUniform4(cs.ProgramId, this.Location, value.X, value.Y, value.Z, value.W);
 	}
 
 	private void ValidateType(UniformType type)
