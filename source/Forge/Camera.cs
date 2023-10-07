dbd4cbc6a768bf45aa0e2ec284e93d95afbd5ee8
JillyMan <artyom543211@gmail.com>
2023-10-07 19:30:43 +0200

    Add camera zoom and adjust speed according to the currentZoom.

--- source/Forge/Camera.cs
+++ source/Forge/Camera.cs
@@ -33,23 +33,35 @@ public class Camera2DController
 {
 	private readonly IMouse mouse;
 
+	private float currentZoom = 1.0f;
 	private Vector2D<float> startMovePosition;
 
 	public CameraData CameraData { get; private set; }
 
 	public float Speed { get; init; } = 1.0f;
 
+	public float ZoomSpeed { get; init; } = 0.1f;
+
 	public Camera2DController(CameraData cameraData, IMouse mouse)
     {
 		CameraData = cameraData;
-		this.mouse = mouse;
 
+		this.mouse = mouse;
 		this.mouse.MouseDown += OnMouseBtnDown;
 		this.mouse.MouseMove += OnMouseMove;
+		this.mouse.Scroll += OnMouseScroll;
 	}
 
 	public void Update(GameTime gameTime)
 	{
+	}
+
+	private void OnMouseScroll(IMouse mouse, ScrollWheel scroll)
+	{
+		float zoomAmount = 1 + ZoomSpeed * scroll.Y;
+		currentZoom *= zoomAmount;
+		var zoomMatrix = Matrix4X4.CreateScale(zoomAmount, zoomAmount, 1);
+		CameraData.Projection *= zoomMatrix;
 	}
 
 	private void OnMouseMove(IMouse mouse, Vector2 vector)
@@ -63,7 +75,8 @@ public class Camera2DController
 			}
 
 			var dir = Vector2D.Normalize(curMousePosition - startMovePosition) * new Vector2D<float>(1, -1);
-			ApplyTranslation(dir * Speed * ForgeGame.Time.DeltaTime);
+			var actualSpeed = Speed / currentZoom;
+			ApplyTranslation(dir * actualSpeed * ForgeGame.Time.DeltaTime);
 			startMovePosition = curMousePosition;
 		}
 	}
